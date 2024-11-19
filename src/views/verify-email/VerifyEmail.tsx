ed31ab9607ee9ff82e45305303c323bc97e733a4
Peter Lanier <peter_lanier@cdlpowersuite.com>
2024-11-19 14:19:44 -0800

    getting more content on the page for verify email

--- src/views/verify-email/VerifyEmail.tsx
+++ src/views/verify-email/VerifyEmail.tsx
@@ -3,6 +3,7 @@ import {
   Button,
   CssBaseline,
   Divider,
+  Link,
   Stack,
   Typography,
 } from "@mui/material";
@@ -13,6 +14,8 @@ import LogoIcon from "../../assets/LogoIconELDT";
 import { brandColors } from "../../styles/brandColors";
 import { useMe } from "../../hooks/useMe";
 import CheckCircleOutlineIcon from "@mui/icons-material/CheckCircleOutline";
+import PhoneIcon from "@mui/icons-material/Phone";
+import EmailIcon from "@mui/icons-material/Email";
 
 interface VerifyEmailProps {
   disableCustomTheme?: boolean;
@@ -35,7 +38,7 @@ const VerifyEmail: React.FC<VerifyEmailProps> = (props: {
       <CssBaseline enableColorScheme />
       <ColorModeSelect sx={{ position: "fixed", top: "1rem", right: "1rem" }} />
       <SignUpContainer direction="column" justifyContent="center">
-        <Card variant="outlined">
+        <Card variant="outlined" sx={{ width: { md: "550px" } }}>
           <CheckCircleOutlineIcon
             sx={{
               fontSize: "100px",
@@ -43,20 +46,12 @@ const VerifyEmail: React.FC<VerifyEmailProps> = (props: {
               margin: "0 auto",
             }}
           />
-          {/* <LogoIcon
-            sx={{
-              width: "250px",
-              height: "55px",
-              color: brandColors.cdlDarkBlue,
-              margin: "0 auto 1rem",
-            }}
-          /> */}
           <Typography component="h1" variant="h4" textAlign="center" mb={2}>
             Verify Your Email
           </Typography>
           <Typography component="p" variant="body1" textAlign="center" mb={2}>
             You have entered <strong>{me?.email}</strong> as the email address
-            for your account. Please check your inbox to verify your email and
+            for your account. Please check your inbox to verify your email to
             complete registration.
           </Typography>
           <Divider />
@@ -78,7 +73,7 @@ const VerifyEmail: React.FC<VerifyEmailProps> = (props: {
           </Button>
           <Divider />
           <Typography component="h1" variant="h6" textAlign="center" mb={2}>
-            Update Email Address?
+            Update email address?
           </Typography>
           <Typography component="p" variant="body1" textAlign="center" mb={2}>
             If you entered the wrong email address, you can update it here.
@@ -86,6 +81,67 @@ const VerifyEmail: React.FC<VerifyEmailProps> = (props: {
           <Button variant="outlined" onClick={handleUpdateEmail} fullWidth>
             Update Email
           </Button>
+          <Divider />
+
+          <Typography component="h1" variant="h6" textAlign="center" mb={2}>
+            Still having trouble?
+          </Typography>
+          <Typography component="p" variant="body1" textAlign="center" mb={2}>
+            Contact customer support.
+          </Typography>
+          <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
+            <LogoIcon
+              sx={{
+                width: "250px",
+                height: "30px",
+                color: brandColors.cdlDarkBlue,
+                margin: "0 auto 1rem",
+              }}
+            />
+            {/* Phone Number */}
+            <Stack
+              direction="row"
+              alignItems="center"
+              justifyContent="center"
+              spacing={1}
+            >
+              <PhoneIcon sx={{ color: "primary.main" }} />
+              <Link
+                href="tel:15092412987"
+                underline="none"
+                sx={{
+                  fontSize: "1rem",
+                  fontWeight: 500,
+                  color: "text.primary",
+                  "&:hover": { color: "primary.main" },
+                }}
+              >
+                1.509.241.2987
+              </Link>
+            </Stack>
+
+            {/* Email Address */}
+            <Stack
+              direction="row"
+              alignItems="center"
+              justifyContent="center"
+              spacing={1}
+            >
+              <EmailIcon sx={{ color: "primary.main" }} />
+              <Link
+                href="mailto:info@cdlpowersuite.com"
+                underline="none"
+                sx={{
+                  fontSize: "1rem",
+                  fontWeight: 500,
+                  color: "text.primary",
+                  "&:hover": { color: "primary.main" },
+                }}
+              >
+                info@cdlpowersuite.com
+              </Link>
+            </Stack>
+          </Box>
         </Card>
       </SignUpContainer>
     </AppTheme>
