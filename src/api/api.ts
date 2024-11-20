d83e0c3f8c4bf7578c9ab9bbbf2014409346a092
Peter Lanier <peter_lanier@cdlpowersuite.com>
2024-11-20 10:43:17 -0800

    add update email and resend verification apis

--- src/api/api.ts
+++ src/api/api.ts
@@ -129,6 +129,35 @@ export const forgotPassword = async ({ email }: { email: string }) => {
   }
 };
 
+// Update Email API
+export const updateEmail = async (userId: string) => {
+  const companyId = process.env.REACT_APP_DEFAULT_COMPANY_ID; // TODO: set env variables to correct values
+  const locationId = 0; // TODO: set to 0 for now
+  try {
+    const response = await apiClient.patch(
+      `https://api.cdlpowersuite.com/api/companies/${companyId}/locations/${locationId}/users/${userId}`
+    );
+    return response.data;
+  } catch (error) {
+    console.error("Error updating email:", error);
+    throw error;
+  }
+};
+
+// Resend Verification Email API
+export const resendVerificationEmail = async (userId: string) => {
+  const companyId = process.env.REACT_APP_DEFAULT_COMPANY_ID; // TODO: set env variables to correct values
+  try {
+    const response = await apiClient.patch(
+      `/companies/${companyId}/users/${userId}/resend-verification-email`
+    );
+    return response.data;
+  } catch (error) {
+    console.error("Error resending verification email:", error);
+    throw error;
+  }
+};
+
 // Me API
 export const getMe = async () => {
   try {
