731c54569d17f028d23745055e8abb8f11087bd9
Michael Williams <mtotheikle@gmail.com>
2024-11-24 10:53:24 -0800

    add sentry

--- src/config/dev.js
+++ src/config/dev.js
@@ -11,6 +11,7 @@ const config = {
         'None': 19,
     },
     stripePublicKey: 'pk_test_51KVilWEqooDHZwmck4VuUymwm3Bw75Fuyryrd0o3MiIlhowWiYpgJg0RgyrNIKufGU4lwTGYZxoIcsSSgP2ZaDmJ00Lb7M2O9G',
+    sentryKey: 'https://3bc1b8796a1a55caad7558064170639b@o4503932842999808.ingest.us.sentry.io/4508354754379776',
 }
 
 export default config;
