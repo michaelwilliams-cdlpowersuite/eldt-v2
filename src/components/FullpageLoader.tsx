39b17de60fa9a4cea297eb6257b0e5fe889b83c0
Michael Williams <mtotheikle@gmail.com>
2024-11-25 10:04:00 -0800

    Add a full page loader for email verification

---
+++ src/components/FullpageLoader.tsx
@@ -0,0 +1,43 @@
+import {Box, LinearProgress} from "@mui/material";
+import LogoIcon from "../assets/LogoIconELDT";
+import {brandColors} from "../styles/brandColors";
+import * as React from "react";
+
+interface FullpageLoaderProps {}
+
+const FullpageLoader: React.FC<FullpageLoaderProps> = () => {
+  return (
+    <div style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      flexDirection: 'column',
+      width: '100%',
+      height: '100%',
+      position: 'fixed',
+      top: 0,
+      left: 0,
+    }}>
+      <Box>
+        <LogoIcon
+          sx={{
+            width: "250px",
+            height: "55px",
+            color: brandColors.cdlDarkBlue,
+          }}
+        />
+        <LinearProgress sx={{
+          width: '100%',
+          backgroundColor: 'white',
+          '& .MuiLinearProgress-bar': {
+            backgroundColor: '#000536',
+            height: '2px'
+          }
+        }}
+        />
+      </Box>
+    </div>
+  )
+}
+
+export default FullpageLoader;
