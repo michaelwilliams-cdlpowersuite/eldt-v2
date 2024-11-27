e35b555d97e9a84f769a7b205bb18c4b6fce2fc6
Peter Lanier <peter_lanier@cdlpowersuite.com>
2024-11-27 10:37:31 -0800

    add logout / sign-out button in app menu

--- src/views/registration/components/AppBar.tsx
+++ src/views/registration/components/AppBar.tsx
@@ -1,10 +1,24 @@
-import { AppBar, Box, Container, Toolbar, Typography } from "@mui/material";
+import {
+  AppBar,
+  Box,
+  Container,
+  IconButton,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 import LocalPhoneIcon from "@mui/icons-material/LocalPhone";
 import { ReactComponent as Logo } from "../../../assets/eldt_white.svg";
+import LogoutIcon from "@mui/icons-material/Logout";
+import { useAuth } from "../../../auth/AuthProvider";
 
 interface RegistrationAppBarProps {}
 
 const RegistrationAppBar: React.FC<RegistrationAppBarProps> = () => {
+  const { clearAuthentication } = useAuth();
+  const handleLogout = () => {
+    clearAuthentication();
+  };
+
   return (
     <AppBar position="fixed" elevation={0} sx={{ bgcolor: "primary.main" }}>
       <Toolbar>
@@ -16,17 +30,30 @@ const RegistrationAppBar: React.FC<RegistrationAppBarProps> = () => {
             justifyContent: "space-between",
           }}
         >
-          <Logo style={{ height: "40px", marginRight: "8px" }} />
           <Box display="flex" alignItems="center">
-            <LocalPhoneIcon />
-            <a
-              href="tel:+15092413987"
-              style={{ textDecoration: "none", color: "inherit" }}
-            >
-              <Typography variant="overline" sx={{ pl: 1 }}>
-                (509) 241-3987
-              </Typography>
-            </a>
+            <Logo style={{ height: "40px", marginRight: "8px" }} />
+          </Box>
+          <Box display="flex" alignItems="center" gap={2}>
+            <Box display="flex" alignItems="center">
+              <LocalPhoneIcon />
+              <a
+                href="tel:+15092413987"
+                style={{ textDecoration: "none", color: "inherit" }}
+              >
+                <Typography variant="overline" sx={{ pl: 1 }}>
+                  (509) 241-3987
+                </Typography>
+              </a>
+            </Box>
+            <Box display="flex" alignItems="center">
+              <IconButton
+                onClick={handleLogout}
+                color="inherit"
+                aria-label="Logout"
+              >
+                <LogoutIcon />
+              </IconButton>
+            </Box>
           </Box>
         </Container>
       </Toolbar>
